c866798d3f797b9825d34be85bc7dedb99196613
paladijmaxim <artemkaklass2006@gmail.com>
2025-03-04 15:19:08 +0300

    сделал дз по проекту 2.1

--- Project/www/route.php
+++ Project/www/route.php
@@ -3,4 +3,5 @@
     return [
         "~^$~" => [src\Controllers\MainController::class, 'main'],
         "~^hello/(.*)$~" =>[src\Controllers\MainController::class, 'sayHello'],
+        "~^bye/(.*)$~" =>[src\Controllers\MainController::class, 'sayBye'],
     ];
