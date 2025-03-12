b21a5dd711f3017d37d03956738a60c43ed54bd9
paladijmaxim <artemkaklass2006@gmail.com>
2025-03-12 10:34:23 +0300

    сдеал страницу приветствия для /hello/, задание 2.2 сделано не полностью

--- Project/templates/main/hello.php
+++ Project/templates/main/hello.php
@@ -1,3 +1,10 @@
-<?php require dirname(__DIR__).'/header.php';?>
-    <h5>Hello, <?=$name;?>!</h5>
-<?php require dirname(__DIR__).'/footer.php';?>
+<?php
+$uri_tek=$_SERVER['REQUEST_URI']; // тек.путь
+if (strpos($uri_tek, '/hello/') !== false) { // Проверка на наличие в пути /hello/
+    require dirname(__DIR__) . '/main/new_header.php';
+} else {
+    require dirname(__DIR__) . '/header.php';
+}
+?>
+<h5>Hello, <?= htmlspecialchars($name); ?>!</h5>
+<?php require dirname(__DIR__) . '/footer.php'; ?>
