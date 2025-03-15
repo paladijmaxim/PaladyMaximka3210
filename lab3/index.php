f5cb63faee52ef75e77ca53a1befa7811e465678
paladijmaxim <artemkaklass2006@gmail.com>
2025-03-15 10:23:29 +0300

    сделал лабу 3.1 файлы

--- lab3/index.php
+++ lab3/index.php
@@ -1,18 +1,41 @@
 <?php
-function transformText(&$words) {
-    $index = 0; 
-    foreach ($words as &$word) {
-        if ($index % 2 == 1) {
-            $word = strtoupper($word);
-        }
-        $index++; 
-    }
-}
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['text'])) {
-    $text = $_POST['text'];
-    $words = explode(' ', $text);
-    transformText($words);
-    $transformedText = implode(' ', $words);
-    echo "<p>$transformedText</p>";
-}
+// function transformText(&$words) {
+//     $index = 0; 
+//     foreach ($words as &$word) {
+//         if ($index % 2 == 1) {
+//             $word = strtoupper($word);
+//         }
+//         $index++; 
+//     }
+// }
+// if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['text'])) {
+//     $text = $_POST['text'];
+//     $words = explode(' ', $text);
+//     transformText($words);
+//     $transformedText = implode(' ', $words);
+//     echo "<p>$transformedText</p>";
+// }
 ?>
+<?php
+    // echo file_get_contents('../text.txt');
+    // var_dump($_SERVER);
+    
+    // file_put_contents('text.txt', "1 2 3 4\r\n5");
+    // rename('text.txt', 'try.txt');
+    // unlink('../text.txt');
+    // var_dump(file_exists('../text.txt'));
+    // var_dump(file_exists('try.txt'));
+
+
+    // $arr = [1,2,3,4,5];
+    // echo filesize('try.txt');
+
+    $a = 2; $b = 5;
+
+print "$a<sup>$b</sup> = $c <BR>";
+
+//Напечатается 2<sup>5</sup> =
+
+require('a_b_req.txt');
+
+print "$a<sup>$b</sup> = $c <BR>";
---
+++ lab3/lab3.1_file/index.php
@@ -0,0 +1,52 @@
+<?php
+
+// numb2(1)
+// file_put_contents('../../test.txt', "12345");
+
+// numb4(2)
+
+// file_put_contents('1.txt', 'hello');
+// file_put_contents('2.txt', 'hello');
+// file_put_contents('3.txt', 'hello');
+
+// $a = ['1.txt', '2.txt', '3.txt'];
+
+// foreach ($a as $w) {
+//     $open = fopen($w, 'a');
+//     if ($open) {
+//         // Записываем восклицательный знак
+//         fwrite($open, '!');
+//     }
+// }
+
+// numb3(3)
+
+// $a = ['1.txt', '2.txt', '3.txt'];
+// $joIn = '';
+
+// foreach ($a as $w) {
+//     if (file_exists($w)) {
+//         $inf = file_get_contents($w);
+//         $joIn .= $inf;
+//     }
+// file_put_contents('new.txt', $joIn);
+// }
+
+//numb6(4)
+
+// $f = '../../test.txt'; 
+// $a = file_get_contents($f);
+// if (is_numeric($a)) {
+//     $square = $a * $a;
+//     file_put_contents($f, $square);
+// }
+ 
+
+// numb7(5)
+
+// Имя файла
+// $f = '../../test.txt';
+// $content = file_get_contents($f);
+// $content .= '!';
+// file_put_contents($f, $content);
+
