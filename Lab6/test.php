3772ec3ed5bb0bff3618941be77bd722dc9d4f55
paladijmaxim <artemkaklass2006@gmail.com>
2025-04-05 10:30:55 +0300

    lab6

--- Lab6/test.php
+++ Lab6/test.php
@@ -8,20 +8,3 @@ session_start();
 
 // for num 6
 
-if (!isset($_SESSION['user_country'])) {
-    header('Location: index.php');
-    exit();
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Ваша страна</title>
-</head>
-<body>
-    <h2>Информация о вашей стране</h2>
-    <p>Вы указали страну: <strong><?= $_SESSION['user_country'] ?></strong></p>
-    <p><a href="index.php">Вернуться на главную</a></p>
-</body>
-</html>
